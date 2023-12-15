354c6a21f46dd8108973ec481393b4bdce8f728e
Safayet Nur <safayetnurelectra@gmail.com>
2023-12-15 09:55:11 -0800

    users page Added

--- src/Pages/UsersPage/Users.tsx
+++ src/Pages/UsersPage/Users.tsx
@@ -1,5 +1,6 @@
 import Layout from "../../components/Layouts/Layout";
 import HeadHook from "../../components/common/HeadHook";
+import { FiUploadCloud, FiPlus } from "react-icons/fi";
 
 const Users = () => {
   return (
@@ -10,7 +11,19 @@ const Users = () => {
         author="Safayet Nur"
         title="Stack-Users"
       />
-      <div>Users</div>
+      <div className="max-w-[1216px] mt-[35px] mx-auto max-h-[40px] flex justify-between items-center">
+        <p className="font-medium text-[24px] text-[#101828]">Users</p>
+        <div className="flex justify-center items-center gap-3">
+          <button className="flex justify-center items-center gap-2 px-4 py-[10px] border border-[#D0D5DD] rounded-[8px] text-[#344054]">
+            <FiUploadCloud className="text-xl" />
+            <p className="font-semibold text-[14px]"> Import</p>
+          </button>
+          <button className="flex justify-center items-center gap-2 px-4 py-[10px] bg-[#7F56D9] text-white rounded-[8px]">
+            <FiPlus className="text-xl" />
+            <p className="font-semibold text-[14px]"> Add User</p>
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 };
