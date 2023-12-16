556f793889c6f87fb9d32fa7d879defdbf4e1335
Safayet Nur <safayetnurelectra@gmail.com>
2023-12-16 05:29:47 -0800

    extra content added

--- src/Pages/TasksPage/Tasks.tsx
+++ src/Pages/TasksPage/Tasks.tsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
 import Layout from "../../components/Layouts/Layout";
 import HeadHook from "../../components/common/HeadHook";
 
 const Tasks = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/todos");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const result = await response.json();
+      setData(result.todos);
+      console.log(result.todos);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       <HeadHook
@@ -10,13 +33,35 @@ const Tasks = () => {
         author="Safayet Nur"
         title="Stack-Tasks"
       />
-      <div className="flex flex-col justify-center items-center mt-48 text-gray-400 ">
-        <p className="font-bold text-3xl tracking-[0.8px]">
-          Welcome to Tasks Page
-        </p>
-        <p className="tracking-[1px] text-xl leading-9">
-          This is Tasks page of stack
-        </p>
+      <div className="max-w-[1216px] mt-[35px] mx-auto h-[40px] flex justify-between items-center">
+        <p className="font-medium text-[24px] text-[#101828]">Tasks</p>
+      </div>
+      <div className="max-w-[1216px] mx-auto grid grid-cols-4 justify-center items-center text-gray-400 gap-6 mt-5 pb-10">
+        {data?.map((e: any, i: number) => (
+          <div
+            key={i}
+            className="rounded-[6px] border border-[#EAECF0] bg-base-100 shadow-md relative"
+          >
+            <div className="card-body !px-4 !py-5 text-[#101828]">
+              {e.todo?.length > 30 ? (
+                <p className="text-[14px] mt-1.5">
+                  {e.todo.slice(0, 30) + "..."}
+                </p>
+              ) : (
+                <p className="text-[14px] mt-1">{e.todo}</p>
+              )}
+            </div>
+            {e.completed == true ? (
+              <div className="badge badge-primary absolute top-2 right-2 text-[10px]">
+                Completed
+              </div>
+            ) : (
+              <div className="badge badge-secondary badge-outline absolute top-2 right-2 text-[10px]">
+                Due
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </Layout>
   );
